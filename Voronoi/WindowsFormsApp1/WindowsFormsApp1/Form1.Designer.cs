7d665fc3434a859f26bc4732bcd8cc5d329c85cb
OleksandrPopilevych <119813015+c11tadeL@users.noreply.github.com>
2024-05-02 01:27:23 +0300

    Add varanoi diagram

---
+++ Voronoi/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MultiThread = new System.Windows.Forms.CheckBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Label();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MultiThread
+            // 
+            this.MultiThread.AutoSize = true;
+            this.MultiThread.Location = new System.Drawing.Point(395, 13);
+            this.MultiThread.Name = "MultiThread";
+            this.MultiThread.Size = new System.Drawing.Size(82, 17);
+            this.MultiThread.TabIndex = 0;
+            this.MultiThread.Text = "MultiThread";
+            this.MultiThread.UseVisualStyleBackColor = true;
+            this.MultiThread.CheckedChanged += new System.EventHandler(this.MultiThread_CheckedChanged);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(548, 10);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(59, 23);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.RandomPoints_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(613, 10);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(59, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.Clear);
+            // 
+            // timer
+            // 
+            this.timer.Location = new System.Drawing.Point(332, 13);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(57, 17);
+            this.timer.TabIndex = 3;
+            this.timer.Text = "\r\n";
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(483, 10);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(59, 23);
+            this.btnDraw.TabIndex = 4;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.Draw);
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(300, 13);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(36, 17);
+            this.time.TabIndex = 5;
+            this.time.Text = "Time:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.MultiThread);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.CheckBox MultiThread;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRandom;
+    }
+}
+
---
+++ Voronoi/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public sealed partial class Form1 : Form
+    {
+        private readonly List<Point> _points = new List<Point>();
+        private readonly List<Color> _colors = new List<Color>();
+        private readonly Random _random = new Random();
+        private readonly Stopwatch _stopwatch;
+        private readonly Bitmap _bitmap;
+        private bool _singleThreadMode;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _stopwatch = new Stopwatch();
+            _bitmap = new Bitmap(ClientSize.Width, ClientSize.Height);
+
+            MaximumSize = new Size(Width, Height);
+            MinimumSize = new Size(Width, Height);
+        }
+
+        private void DrawVertices(Graphics graphics)
+        {
+            foreach (Point point in _points)
+            {
+                graphics.FillEllipse(Brushes.Black, point.X - 3, point.Y - 3, 5, 5);
+            }
+        }
+
+        private void Form_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+
+            graphics.DrawImage(_bitmap, 0, 0);
+
+            DrawVertices(graphics);
+        }
+
+        private void Form_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point clickedPoint = e.Location;
+
+                bool pointRemoved = false;
+
+                for (int i = 0; i < _points.Count; i++)
+                {
+                    if (Distance(clickedPoint.X, clickedPoint.Y, _points[i]) <= 5)
+                    {
+                        _points.RemoveAt(i);
+                        _colors.RemoveAt(i);
+
+                        pointRemoved = true;
+                        break;
+                    }
+                }
+
+                if (!pointRemoved)
+                {
+                    _points.Add(clickedPoint);
+                    _colors.Add(Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256)));
+                }
+
+                Invalidate();
+            }
+        }
+
+        private void DrawSingleThread()
+        {
+            using (Graphics graphics = Graphics.FromImage(_bitmap))
+            {
+                for (int x = 0; x < _bitmap.Width; x++)
+                {
+                    for (int y = 0; y < _bitmap.Height; y++)
+                    {
+                        Point closestPoint = FindClosestPoint(x, y);
+
+                        int index = _points.IndexOf(closestPoint);
+
+                        graphics.FillRectangle(new SolidBrush(_colors[index]), x, y, 1, 1);
+                    }
+                }
+            }
+
+        }
+
+        //private void DrawMultiThread()
+        //{
+        //    int segmentWidth = (int)Math.Ceiling((double)_bitmap.Width / Environment.ProcessorCount);
+        //    int segmentHeight = _bitmap.Height;
+
+        //    List<Task> tasks = new List<Task>();
+
+        //    for (int t = 0; t < Environment.ProcessorCount; t++)
+        //    {
+        //        int startX = t * segmentWidth;
+        //        int endX = Math.Min((t + 1) * segmentWidth, _bitmap.Width);
+
+        //        tasks.Add(Task.Run(() =>
+        //        {
+        //            using (Graphics graphics = Graphics.FromImage(_bitmap))
+        //            {
+        //                for (int x = startX; x < endX; x++)
+        //                {
+        //                    for (int y = 0; y < segmentHeight; y++)
+        //                    {
+        //                        Point closestPoint = FindClosestPoint(x, y);
+
+        //                        int index = _points.IndexOf(closestPoint);
+
+        //                        lock (graphics)
+        //                        {
+        //                            graphics.FillRectangle(new SolidBrush(_colors[index]), x, y, 1, 1);
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //        }));
+        //    }
+
+        //    Task.WaitAll(tasks.ToArray());
+        //}
+        private void DrawMultiThread()
+        {
+            int segmentWidth = (int)Math.Ceiling((double)_bitmap.Width / Environment.ProcessorCount);
+            int segmentHeight = _bitmap.Height;
+
+            List<Bitmap> bitmaps = new List<Bitmap>();
+
+            for (int t = 0; t < Environment.ProcessorCount; t++)
+            {
+                bitmaps.Add(new Bitmap(_bitmap.Width, _bitmap.Height));
+            }
+
+            List<Task> tasks = new List<Task>();
+
+            for (int t = 0; t < Environment.ProcessorCount; t++)
+            {
+                int startX = t * segmentWidth;
+                int endX = Math.Min((t + 1) * segmentWidth, _bitmap.Width);
+
+                Bitmap localBitmap = bitmaps[t];
+
+                tasks.Add(Task.Run(() =>
+                {
+                    using (Graphics graphics = Graphics.FromImage(localBitmap))
+                    {
+                        for (int x = startX; x < endX; x++)
+                        {
+                            for (int y = 0; y < segmentHeight; y++)
+                            {
+                                Point closestPoint = FindClosestPoint(x, y);
+
+                                int index = _points.IndexOf(closestPoint);
+
+                                graphics.FillRectangle(new SolidBrush(_colors[index]), x, y, 1, 1);
+                            }
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            using (Graphics mainGraphics = Graphics.FromImage(_bitmap))
+            {
+                foreach (Bitmap localBitmap in bitmaps)
+                {
+                    mainGraphics.DrawImage(localBitmap, 0, 0);
+                }
+            }
+        }
+
+
+        private Point FindClosestPoint(int x, int y)
+        {
+            Point closestPoint = _points[0];
+
+            double minDistance = Distance(x, y, closestPoint);
+
+            for (int i = 1; i < _points.Count; i++)
+            {
+                double distance = Distance(x, y, _points[i]);
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestPoint = _points[i];
+                }
+            }
+
+            return closestPoint;
+        }
+
+        private double Distance(int x1, int y1, Point point)
+        {
+            return Math.Sqrt(Math.Pow(x1 - point.X, 2) + Math.Pow(y1 - point.Y, 2));
+        }
+
+        private void MultiThread_CheckedChanged(object sender, EventArgs e)
+        {
+            _singleThreadMode = MultiThread.Checked;
+        }
+
+        private void Draw(object sender, EventArgs e)
+        {
+            if (_points.Count == 0)
+            {
+                MessageBox.Show("There are no vertices on the form!");
+                return;
+            }
+
+            _stopwatch.Restart();
+
+            if (!_singleThreadMode)
+            {
+                DrawSingleThread();
+            }
+            else
+            {
+                DrawMultiThread();
+            }
+
+            _stopwatch.Stop();
+            timer.Text = $"{_stopwatch.ElapsedMilliseconds} ms";
+
+            Invalidate();
+        }
+
+        private void RandomPoints_Click(object sender, EventArgs e)
+        {
+            Clear(sender, e);
+
+            int numberPoints = 500;
+
+            for (int i = 0; i < numberPoints; i++)
+            {
+                _points.Add(new Point(_random.Next(ClientSize.Width), _random.Next(ClientSize.Height)));
+                _colors.Add(Color.FromArgb(_random.Next(256), _random.Next(256), _random.Next(256)));
+            }
+
+            Invalidate();
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            using (Graphics graphics = Graphics.FromImage(_bitmap))
+            {
+                _points.Clear();
+                _colors.Clear();
+                graphics.Clear(Color.White);
+
+                DrawVertices(graphics);
+            }
+
+            Invalidate();
+        }
+    }
+}
